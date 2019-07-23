94b4f379228516744b542648d2398e97010bd59a
unknown <dominik_p@wp.pl>
2019-07-23 20:24:32 +0200

    Changed SingleProduct component

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 import Loading from './Loading';
 
@@ -19,10 +20,27 @@ class SingleProduct extends Component {
                     if(loading) return <Loading />
                     if(error) return <p>{error.message}</p>
 
+                    const { name, desc, price } = data.product;
+
                     return(
-                        <div>
-                            {data.product.name}
-                        </div>
+                        <Fragment>
+                            <div className="single-product">
+                                <div className="product-header">
+                                    <span>{name}</span>
+                                    <img src="https://via.placeholder.com/300" alt="product" />
+                                </div>
+                                <div className="product-body">
+                                    <p>{desc}</p>
+                                    <div>
+                                        <span>Cena: <strong>{price}</strong> PLN</span>
+                                        <button className="btn btn-success">Add to Cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="single-product-back">
+                                <Link to="/products" className="btn btn-primary" >Produkty</Link>
+                            </div>
+                        </Fragment>
                     );
                 }}
             </Query>
